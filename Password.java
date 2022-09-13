c6c2d4e2083831eba3da6c3ca2a64f86d1897728
Srikanthnaid <ksru044gmail.com>
2022-09-13 19:50:07 +0530

    password

---
+++ Password.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Password {
+	Scanner s = new Scanner(System.in);
+
+	public void passWord() {
+		System.out.println("Enter password: ");
+		String PassWord = s.next();
+		Boolean result = Pattern.matches("[a-z]{8}", PassWord);
+		if (result) {
+			System.out.println("valid");
+		} else {
+			System.out.println("please enter correctly");
+		}
+	}
+
+	public static void main(String[] args) {
+		Password password = new Password();
+		password.passWord();
+	}
+}
